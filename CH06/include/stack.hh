fecf2df1aa2b05487887c54fb1a5fd63385b2c5a
Yeho Gwon <devstita@gmail.com>
2023-06-24 17:48:24 +0900

    linked-list-based stack

--- CH06/include/stack.hh
+++ CH06/include/stack.hh
@@ -32,4 +32,42 @@ public:
     }
 };
 
+template <typename T>
+class LinkedListStack {
+private: 
+    struct Node {
+        T data;
+        Node *next;
+
+        Node() : next(nullptr) {}
+        Node(T data) : data(data), next(nullptr) {}
+        Node(T data, Node *next) : data(data), next(next) {}
+    };
+
+    Node *top;
+public: 
+    LinkedListStack() : top(nullptr) {}
+
+    void push(T val) {
+        top = new Node(val, top);
+    }
+
+    T pop() {
+        T val = peek();
+        Node *p = top;
+        top = top->next;
+        delete p;
+        return val;
+    }
+
+    T peek() {
+        if (empty()) throw std::out_of_range("The stack is empty.");
+        return top->data;
+    }
+
+    bool empty() {
+        return top == nullptr;
+    }
+};
+
 #endif // __STACK_HH__
--- CH06/stack.main.cc
+++ CH06/stack.main.cc
@@ -2,11 +2,17 @@
 #include "stack.hh"
 
 int main() {
-    ArrayStack<int> stack;
-
-    for (int i = 1; i <= 5; i++) stack.push(i);
-
-    while (!stack.empty()) std::cout << stack.pop() << std::endl;
+    std::cout << " ==== Array-based Stack ==== " << std::endl;
+    ArrayStack<int> a_stack;
+    for (int i = 1; i <= 5; i++) a_stack.push(i);
+    while (!a_stack.empty()) std::cout << a_stack.pop() << " ";
+    std::cout << std::endl;
+    
+    std::cout << " ==== Linked-list-based Stack ==== " << std::endl;
+    LinkedListStack<int> l_stack;
+    for (int i = 1; i <= 5; i++) l_stack.push(i);
+    while (!l_stack.empty()) std::cout << l_stack.pop() << " ";
+    std::cout << std::endl;
 
     return 0;
 }
