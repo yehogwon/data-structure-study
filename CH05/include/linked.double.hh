97dea7a07872b48db4209f55639a85499e337889
Yeho Gwon <devstita@gmail.com>
2023-06-27 13:31:24 +0900

    doubly linked list

--- CH05/include/linked.double.hh
+++ CH05/include/linked.double.hh
@@ -1,4 +1,67 @@
 #ifndef __LINKED_DOUBLE_HH__
 #define __LINKED_DOUBLE_HH__
 
+template <typename T>
+class DoublyLinkedList {
+private: 
+    struct Node { // Doubly linked list
+        T data;
+        Node *prev, *next;
+
+        Node() : prev(nullptr), next(nullptr) {}
+        Node(T data, Node *prev=nullptr, Node *next=nullptr) : data(data), prev(prev), next(next) {}
+    };
+
+    Node *head, *cur;
+    int n_data;
+
+    bool get(T &value) {
+        if (!cur) return false;
+        value = cur->data;
+        return true;
+    }
+public: 
+    DoublyLinkedList() : head(nullptr), cur(nullptr), n_data(0) {}
+
+    ~DoublyLinkedList() {
+        while (head) {
+            Node *p = head;
+            head = head->next;
+            delete p;
+        }
+    }
+
+    void insert(T value) {
+        Node *p = new Node(value, nullptr, head);
+        if (head) head->prev = p;
+        head = p;
+        n_data++;
+    }
+
+    bool first(T &value) {
+        cur = head;
+        return get(value);
+    }
+
+    bool next(T &value) {
+        if (!cur || !(cur->next)) return false;
+        cur = cur->next;
+        return get(value);
+    }
+
+    bool previous(T &value) {
+        if (!cur || !(cur->prev)) return false;
+        cur = cur->prev;
+        return get(value);
+    }
+
+    int count() {
+        return n_data;
+    }
+
+    bool empty() {
+        return n_data == 0;
+    }
+};
+
 #endif // __LINKED_DOUBLE_HH__
--- CH05/linked.double.main.cc
+++ CH05/linked.double.main.cc
@@ -1,6 +1,20 @@
 #include <iostream>
 #include "linked.double.hh"
 
+using List = DoublyLinkedList<int>;
+
 int main() {
+    List list;
+    int data;
+
+    for (int i = 1; i <= 8; i++) list.insert(i);
+
+    if (list.first(data)) {
+        std::cout << data << " ";
+        while (list.next(data)) std::cout << data << " ";
+        while (list.previous(data)) std::cout << data << " ";
+        std::cout << std::endl;
+    }
+
     return 0;
 }
