a7a69692519fa15cb62577a319bf1654284ad18f
Yeho Gwon <devstita@gmail.com>
2023-07-01 17:15:00 +0900

    implement numeric tools

--- CH08/include/tree.calc.hh
+++ CH08/include/tree.calc.hh
@@ -2,18 +2,22 @@
 #define __TREE_CALC_HH__
 
 #include <string>
+#include <stdexcept>
 #include "stack.hh"
 #include "binary.tree.hh"
 #include "binary.tree.traverse.hh"
 
 bool is_operator(char c);
-bool is_numeric(char c);
-bool is_dp(char c);
-float numberify(const std::string::const_iterator &begin, const std::string::const_iterator &end);
 int operator_precedence(char c);
 
-std::string infix_to_postfix(const std::string &expression);
+bool is_numeric(char c);
+bool is_dp(char c);
+bool is_float(char c);
 
-float calculate(const nodeptr<*> &root);
+int intify(char c);
+float numberify(const std::string::const_iterator &begin, const std::string::const_iterator &end);
+
+std::string infix_to_postfix(const std::string &expression);
+float calculate(const nodeptr<?> &root);
 
 #endif // __TREE_CALC_HH__
--- CH08/src/tree.calc.cc
+++ CH08/src/tree.calc.cc
@@ -1,1 +1,60 @@
 #include "tree.calc.hh"
+
+bool is_operator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')';
+}
+
+int operator_precedence(char c) {
+    if (!is_operator(c)) throw std::invalid_argument("Not an operator");
+    switch (c) {
+        case '*':
+        case '/':
+            return 5;
+        case '+':
+        case '-':
+            return 3;
+        case '(': 
+        case ')':
+            return 1;
+    }
+    return -1;
+}
+
+bool is_numeric(char c) {
+    return c >= '0' && c <= '9';
+}
+
+bool is_dp(char c) {
+    return c == '.';
+}
+
+bool is_float(char c) {
+    return is_numeric(c) || is_dp(c);
+}
+
+int intify(char c) {
+    return c - '0';
+}
+
+float numberify(const std::string::const_iterator &begin, const std::string::const_iterator &end) {
+    float number = 0;
+    int decimal = 0;
+    for (auto it = begin; it != end && is_float(*it); it++) {
+        if (is_dp(*it)) {
+            if (decimal) throw std::invalid_argument("Invalid number");
+            decimal = 1;
+            continue;
+        }
+
+        if (is_numeric(*it)) {
+            if (decimal) {
+                decimal *= 10;
+                number += intify(*it) / (decimal * 1.0);
+            } else {
+                number *= 10;
+                number += intify(*it);
+            }
+        }
+    }
+    return number;
+}
