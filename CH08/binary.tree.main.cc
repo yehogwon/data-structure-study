44f3bb55d8c7218dab3fbb7edc463e6a99c968c1
Yeho Gwon <devstita@gmail.com>
2023-07-01 13:34:52 +0900

    binary tree implementation

--- CH08/binary.tree.main.cc
+++ CH08/binary.tree.main.cc
@@ -1,6 +1,30 @@
 #include <iostream>
 #include "binary.tree.hh"
 
+using inodeptr = nodeptr<int>;
+
 int main() {
+    inodeptr bt1 = create_node<int>(), bt2 = create_node<int>(), bt3 = create_node<int>(), bt4 = create_node<int>();
+
+    bt1->data = 1;
+    bt2->data = 2;
+    bt3->data = 3;
+    bt4->data = 4;
+
+    bt1->left = bt2;
+    bt1->right = bt3;
+    bt2->left = bt4;
+    
+    // print the tree bt1 in the shape of tree
+    std::cout << " ---- TREE ---- " << std::endl;
+    std::cout << "      " << bt1->data << std::endl;
+    std::cout << "     / \\" << std::endl;
+    std::cout << "    " << bt2->data << "   " << bt3->data << std::endl;
+    std::cout << "   /" << std::endl;
+    std::cout << "  " << bt4->data << std::endl;
+
+    std::cout << bt1->left->data << std::endl;
+    std::cout << bt1->left->left->data << std::endl;
+
     return 0;
 }
--- CH08/include/binary.tree.hh
+++ CH08/include/binary.tree.hh
@@ -1,4 +1,30 @@
 #ifndef __BINARY_TREE_HH__
 #define __BINARY_TREE_HH__
 
+#include <memory>
+
+template <typename T>
+struct __BinaryTreeNode;
+
+template <typename T>
+class nodeptr : public std::shared_ptr<__BinaryTreeNode<T>> {
+public: 
+    nodeptr() : std::shared_ptr<__BinaryTreeNode<T>>(new __BinaryTreeNode<T>) {}
+    nodeptr(__BinaryTreeNode<T> *ptr) : std::shared_ptr<__BinaryTreeNode<T>>(ptr) {}
+};
+
+template <typename T>
+struct __BinaryTreeNode {
+    T data;
+    nodeptr<T> left, right;
+
+    __BinaryTreeNode() : left(nullptr), right(nullptr) {}
+    __BinaryTreeNode(T data, nodeptr<T> *left=nullptr, nodeptr<T> *right=nullptr) : data(data), left(left), right(right) {}
+};
+
+template <typename T>
+nodeptr<T> create_node() {
+    return nodeptr<T>(new __BinaryTreeNode<T>);
+}
+
 #endif // __BINARY_TREE_HH__
