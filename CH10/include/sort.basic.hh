67680673f6f058f2267a124f4bb5b221961f6b5d
Yeho Gwon <devstita@gmail.com>
2023-07-06 15:08:58 +0900

    implement selection sort

--- CH10/include/sort.basic.hh
+++ CH10/include/sort.basic.hh
@@ -16,4 +16,15 @@ void bubble_sort(T *arr, int n) {
                 swap(arr[j], arr[j + 1]);
 }
 
+template <typename T>
+void selection_sort(T *arr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[min])
+                min = j;
+        if (i != min) swap(arr[i], arr[min]);
+    }
+}
+
 #endif // __BASIC_SORT_HH__
--- CH10/sort.main.cc
+++ CH10/sort.main.cc
@@ -9,10 +9,18 @@ void print(T *arr, int n) {
 }
 
 int main() {
-    int arr[4] = {3, 2, 4, 1};
-    
-    bubble_sort(arr, 4);
-    print(arr, 4);
+    {
+        int arr[4] = {3, 2, 4, 1};
+        std::cout << " === Bubble Sort === " << std::endl;
+        bubble_sort(arr, 4);
+        print(arr, 4);
+    }
+    {
+        int arr[4] = {3, 2, 4, 1};
+        std::cout << " === Selection Sort === " << std::endl;
+        selection_sort(arr, 4);
+        print(arr, 4);
+    }
     
     return 0;
 }
