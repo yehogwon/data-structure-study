56ca209a1f9ce6f14af8ea365862c8a4d1dbc21c
Yeho Gwon <devstita@gmail.com>
2023-07-14 01:42:15 +0900

    add testcases

--- CH11/binary.search.tree.main.cc
+++ CH11/binary.search.tree.main.cc
@@ -4,16 +4,19 @@
 int main() {
     BinarySearchTree<item<int, double>> bst;
 
+    bst.insert({0, 0.12});
     bst.insert({1, 1.34});
-    bst.insert({2, 2.96});
     bst.insert({3, 3.14});
+    bst.insert({4, 4.56});
+    bst.insert({7, 7.22});
+    bst.insert({8, 8.37});
 
-    for (int key : {0, 1, 2, 3, 4, 5}) {
+    for (int key : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}) {
         try {
             auto &item = bst.search(key);
-            std::cout << "[     ITEM FOUND     ] " << item.key << " " << item.value << std::endl;
+            std::cout << "[      ITEM FOUND     ] " << item.key << " " << item.value << std::endl;
         } catch (...) {
-            std::cout << "[ ITEM IS NOT FOUND  ] " << key << std::endl;
+            std::cout << "[  ITEM IS NOT FOUND  ] " << key << std::endl;
         }
     }
 
